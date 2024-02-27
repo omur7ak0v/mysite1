3e189bb52bd244ee2f6d9d465bb44e874a3ee4fa
Muratbek <omur7ak0v@gmail.com>
2024-02-27 18:00:18 +0600

    My Commit 1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// App.js
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        {/* Ваш основной контент здесь */}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
